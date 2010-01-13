59283a3ca976f0608d0caf32f70dec361cdfc6e2
Andew Heiss <andrew@andrewheiss.com>
2010-01-13 21:29:56 +0200

    Votes are associated with users

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,9 @@
 class VotesController < ApplicationController
   def create
-    @vote = Vote.create(:contestant_id => params[:contestant_id], :episode_id=> params[:episode_id])
+    @vote = Vote.create(
+      :user_id => @current_user, 
+      :contestant_id => params[:contestant_id], 
+      :episode_id=> params[:episode_id])
     @contestant = Contestant.find(params[:contestant_id])
     
     respond_to do |format|
