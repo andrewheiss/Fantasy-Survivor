731a7ec4b1b14cb5058768db7cdca8561f0e267b
Andew Heiss <andrew@andrewheiss.com>
2010-01-25 20:33:50 +0100

    Added basic vote/episode/user uniqueness check

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :episode
   belongs_to :user
   validates_presence_of :contestant_id, :episode_id
+  
+  validates_uniqueness_of :user_id, :scope => :episode_id
 end
